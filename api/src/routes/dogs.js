d9bb5ccd066e164a5cfcdcd7f65ca396be64ad88
maurohaidr <maurohaidr@gmailcom>
2021-06-07 11:05:01 -0300

    -fix- ya no rompe cuando se busca un perro que no tiene propiedad imagen en la api

--- api/src/routes/dogs.js
+++ api/src/routes/dogs.js
@@ -5,45 +5,43 @@ const { Op } = require("sequelize");
 const dogs = function(req, res) {
     if(req.query.name){
         const name = req.query.name;
+        const ret = [];
         axios.get('https://api.thedogapi.com/v1/breeds/search?q='+name).then(response => {
-            if(response.data.length > 8) response.data.splice(8, response.data.length-1)           
-            const ret = [];            
+            /* if(response.data.length > 8) response.data.splice(8, response.data.length-1)    */                          
             let img = '';
-            response.data.forEach(e => {
-                axios.get('https://api.thedogapi.com/v1/breeds').then(resp =>{
-                    const filtrado = resp.data.filter(i => i.image.id === e.reference_image_id)[0]
+            axios.get('https://api.thedogapi.com/v1/breeds').then(resp =>{
+                response.data.forEach(e => {                
+                    let filtrado = {image:{ url: 'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/c54673e0-9fa9-408d-8f3e-af6c66b31179/dcjz6zw-0462b457-e292-4734-b8e6-fce402dc9bd9.jpg/v1/fill/w_800,h_800,q_75,strp/spunky___rocko_s_modern_life_by_stuf123_dcjz6zw-fullview.jpg?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9ODAwIiwicGF0aCI6IlwvZlwvYzU0NjczZTAtOWZhOS00MDhkLThmM2UtYWY2YzY2YjMxMTc5XC9kY2p6Nnp3LTA0NjJiNDU3LWUyOTItNDczNC1iOGU2LWZjZTQwMmRjOWJkOS5qcGciLCJ3aWR0aCI6Ijw9ODAwIn1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.87hM-7B9Mbqqug-3K1WQhByQB6dgt2dFLP0Wjm1J0_Q'}}
+                    if(e.reference_image_id) filtrado = resp.data.filter(i => i.image.id === e.reference_image_id)[0]
                     img = filtrado.image.url  //cambio el id por la url de la imagen
                     ret.push({
                        imagen: img,
                        nombre: e.name,
                        temperamento: e.temperament 
                        })
-                 }).then(
-                    Raza.findAll({ include: Temperamento, where: { nombre:{ [Op.like]: `%${name}%`  } } }).then(resultado => {
-                        resultado.forEach(f => {
-                            let temperamento = '';
-                            f.temperamentos.forEach(i =>{
-                                temperamento = temperamento.concat(i.nombre + ', ') //junto el array de temperamentos en un string
-                            })
-                            temperamento = temperamento.slice(0, temperamento.length-2) //elimino el ultimo ', '
-                            ret.push({
-                                nombre: f.nombre,
-                                imagen: f.imagen,
-                                temperamento: temperamento 
-                            })
-                        })        
-                    })
-                 ).then(() =>{
-                    return res.json(ret)
+                 })
+                 Raza.findAll({ include: Temperamento, where: { nombre:{ [Op.like]: `%${name}%`  } } }).then(resultado => {
+                    resultado.forEach(f => {
+                        let temperamento = '';
+                        f.temperamentos.forEach(i =>{
+                            temperamento = temperamento.concat(i.nombre + ', ') //junto el array de temperamentos en un string
+                        })
+                        temperamento = temperamento.slice(0, temperamento.length-2) //elimino el ultimo ', '
+                        ret.push({
+                            nombre: f.nombre,
+                            imagen: f.imagen,
+                            temperamento: temperamento 
+                        })
+                    })        
                 })
+                return res.json(ret)                
             })
-            
         })
         .catch(error => res.status(500).json(error))
     }
     else
     axios.get('https://api.thedogapi.com/v1/breeds').then(response => {
-        if(response.data.length > 8) response.data.splice(8, response.data.length-1)
+        /* if(response.data.length > 8) response.data.splice(8, response.data.length-1) */
         const ret = [];
         response.data.forEach(e => ret.push({
             imagen: e.image.url,
--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -16,7 +16,22 @@ import { connect } from "react-redux";
 
 const Home = (props) => {
     const [raza, setRaza] = useState('');
+    const [filter, setFilter] = useState('Raza')
+    const [order, setOrder] = useState('a-z')
 
+    /* useEffect(() => {  
+      console.log(props.razas)  
+      {props.razas && props.razas.map((e) => {
+        return(            
+            <div className='razaCard'>
+            <span>Nombre:</span><span>{e.nombre}</span>
+            <span>Temperamento:</span>{e.temperamento}
+            <img src={e.imagen} width="200" height="200" alt="" />
+            </div>
+            )}
+        )             
+      }
+    }, [props.razas]) */
     let handleChange = function (e) {
         setRaza(e.target.value);
       }
@@ -25,19 +40,20 @@ const Home = (props) => {
         e.preventDefault(); 
         props.getRazas(raza)
       }
-      /* useEffect(() =>{
-        if(props.razas){
-            props.razas.forEach(e => {
-                return(
-                <div>
-                <span>Nombre:</span><span>{e.nombre}</span>
-                <span>Temperamento:</span>{e.temperamento}
-                <img src={e.imagen} width="200" height="200" alt="" />
-                </div>
-                )
-            })
-        } 
-    }, [props.razas, handleSubmit]) */
+    let toggleFilter = function(e){
+      e.preventDefault();
+      if(filter === 'Raza')
+      setFilter('Temperamento')
+      else setFilter('Raza')
+    }
+    let toggleOrder = function(e){
+      e.preventDefault(); 
+      if(order === 'a-z'){
+      setOrder('Peso')
+      props.razas && props.razas.sort((a, b) => (a.weight.metric > b.weight.metric) ? 1 : -1)
+      }else setOrder('a-z')
+      props.razas && props.razas.sort((a, b) => (a.name > b.name) ? 1 : -1)
+    }
 
     return (
       <div className='homeCointainter'>
@@ -51,11 +67,14 @@ const Home = (props) => {
             />
           </div>
           <button type="submit">BUSCAR</button>
-        </form>   
-                
+        </form> 
+        <span>Filtro:</span>
+        <button onClick={(e) => toggleFilter(e)}>{filter}</button>
+        <span>Orden:</span>
+        <button onClick={(e) => toggleOrder(e)}>{order}</button>
         {props.razas && props.razas.map((e) => {
             return(            
-                <div>
+                <div className='razaCard'>
                 <span>Nombre:</span><span>{e.nombre}</span>
                 <span>Temperamento:</span>{e.temperamento}
                 <img src={e.imagen} width="200" height="200" alt="" />
@@ -64,7 +83,8 @@ const Home = (props) => {
             )             
         }
         
-        {/* <div className='bar'><span>Buscar</span><button>raza/temperamento</button><input></input><button>ordenar por peso/alfabeticamente</button><button>ascendente/descendente</button></div> */}
+        {/* <div className='bar'><span>Buscar</span><button>raza/temperamento</button><input></input>
+        <button>ordenar por peso/alfabeticamente</button><button>ascendente/descendente</button></div> */}
         
       </div>      
     )
